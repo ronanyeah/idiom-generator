c1dd12cd3a770f181e8192a4fee705445772fb97
ronan <ronan_mccabe@protonmail.com>
2017-05-07 17:46:24 +0300

    remove yarn for simplicity

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf node_modules
-yarn --production
+npm install --production
 zip -FSr ./lambda.zip ./node_modules/ ./index.js ./src/idiom.js ./src/twitter.js ./src/helpers.js ./content/nouns.json ./content/qualifiers.json ./content/conditions.json
 aws lambda update-function-code --function-name idiom-tweeter --zip-file fileb://./lambda.zip
 rm ./lambda.zip
